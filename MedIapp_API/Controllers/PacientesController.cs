ff07502dd05916fdca4987e24c3c2f8812475ec8
Pierre Grandett <pierre.grandett@startleftsecurity.com>
2024-06-03 00:03:09 -0500

    Agregar archivos de proyecto.

---
+++ MedIapp_API/Controllers/PacientesController.cs
@@ -0,0 +1,94 @@
+﻿using MedIapp_API.Datos;
+using MedIapp_API.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+
+namespace MedIapp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<IEnumerable<PacientesDTO>> GetPacientes()
+        {
+            return Ok(PacientesMockData.PacientesList);
+        }
+
+
+        [HttpGet("id:int", Name = "GetPaciente")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<PacientesDTO> GetPacientes(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var paciente = PacientesMockData.PacientesList.FirstOrDefault(v => v.Id == id);
+
+            if (paciente == null) {
+
+                return NotFound();
+            }
+            return Ok(paciente);
+        }
+
+        [HttpPost("CrearPaciente")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<PacientesDTO> CrearPaciente([FromBody] PacientesDTO pacientesDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Verificar si ya existe un paciente con el mismo ID
+            if (PacientesMockData.PacientesList.FirstOrDefault(v => v.Id == pacientesDTO.Id) != null)
+            {
+                ModelState.AddModelError("Id", "Registro existente");
+                return BadRequest(ModelState);
+            }
+
+            // Verificar si ya existe un paciente con la misma combinación de ID y nombre
+            if (PacientesMockData.PacientesList.FirstOrDefault(v => v.Id == pacientesDTO.Id && v.Nombre == pacientesDTO.Nombre) != null)
+            {
+                ModelState.AddModelError("IdNombre", "Registro existente con el mismo ID y nombre");
+                return BadRequest(ModelState);
+            }
+
+            // Si pasa ambas validaciones, se puede crear el nuevo registro
+            PacientesMockData.PacientesList.Add(pacientesDTO);
+            return Ok("Registro creado exitosamente!");
+
+
+            if (pacientesDTO == null)
+            {
+                return BadRequest(pacientesDTO);
+            }
+
+            if (pacientesDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            pacientesDTO.Id = PacientesMockData.PacientesList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+            PacientesMockData.PacientesList.Add(pacientesDTO);
+
+            return CreatedAtRoute("GetPaciente", new { Id = pacientesDTO.Id, pacientesDTO });
+
+        }
+    }
+
+
+
+}
+
+
+
+
---
+++ MedIapp_API/Modelos/PacientesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedIapp_API.Modelos
+{
+    public class PacientesDTO
+    {
+        
+        
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
